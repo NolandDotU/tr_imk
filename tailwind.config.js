946f6c195563a55c657b1ec07448c8e1bb49a080
Noland <88490545+NolandDotU@users.noreply.github.com>
2024-06-21 10:26:31 +0700

    page logib

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         'primary': '#97A97C',
       },
+      backgroundImage: {
+        'login-img': "url('https://s3-alpha-sig.figma.com/img/c51e/9043/f3797927c60b3248c3723f14526075a3?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Tlvsfyws-A7SLQYj6Wrqbd0N~yGXaj8W1fP2wtHEFzBlXpcRVHwYER7EItV3DclHo715KmGv8yyIdUA-wAHaGBQl~KsF44vUsM0IUjsrQqRgZBdIi7ofsod85taDpwhjn-qLIe2Rr3BWbKIVgF5GPMoEms5ojrvrF-dMPPFeweUg2S~J0do4utELH29~rH18cCXCTdtFd6bMryNG7EwFqKCIhnpE4sYcpF0h6-UHuzhNIgpNSTjfC980dVvNqY1PJOgXvbRlxT3h~kfGhd6BE62IAsku~qiVVp7B0-P6VKlxrTBLYCmLWkVjrQYz1LG3jCXr~b0HhC9qaQpWfj3T4Q__')"
+      },
     },
   },
   plugins: [
